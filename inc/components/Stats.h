366ffe5155585b26371f0d48e5140e2b1083ccbe
focusfail <hubigpaul@gmail.com>
2024-11-05 15:19:40 +0100

    feat: added stats component

---
+++ inc/components/Stats.h
@@ -0,0 +1,12 @@
+#ifndef STATS_H
+#define STATS_H
+
+struct Stats
+{
+    float health = 100.0f;
+    float reach = 2.0f;      // Interaction-reach in blocks
+    float speed = 500.0f;    // Base speed
+    float sprintMul = 1.75f; // Sprint speed multiplier
+};
+
+#endif // STATS_H
